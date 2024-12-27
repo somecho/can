96e609f7232cb3fdf806fb30d0f034bc09489477
Somē Cho <itssomicho@gmail.com>
2024-12-27 16:48:24 +0100

    implement application and window class

---
+++ src/can.cpp
@@ -0,0 +1,193 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_error.h>
+#include <MidiParser/Parser.hpp>
+#include <algorithm>
+#include <cmath>
+#include <format>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+// https://github.com/openframeworks/openFrameworks/blob/0.12.0/libs/openFrameworks/math/ofMath.cpp#L78
+float map(float value, float inputMin, float inputMax, float outputMin,
+          float outputMax, bool clamp = true) {
+  if (std::abs(inputMin - inputMax) < std::numeric_limits<float>::epsilon()) {
+    return outputMin;
+  } else {
+    float outVal =
+        ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) +
+         outputMin);
+
+    if (clamp) {
+      if (outputMax < outputMin) {
+        if (outVal < outputMax)
+          outVal = outputMax;
+        else if (outVal > outputMin)
+          outVal = outputMin;
+      } else {
+        if (outVal > outputMax)
+          outVal = outputMax;
+        else if (outVal < outputMin)
+          outVal = outputMin;
+      }
+    }
+    return outVal;
+  }
+}
+
+template <class... Ts>
+struct overload : Ts... {
+  using Ts::operator()...;
+};
+
+struct QuantizedNote {
+  uint8_t key;
+  uint32_t timeStart, timeEnd;
+};
+
+std::vector<QuantizedNote> quantizeMIDI(const MidiParser::MidiFile& data) {
+  using namespace MidiParser;
+
+  std::unordered_map<uint8_t, uint32_t> noteOnTimes;
+  std::vector<QuantizedNote> notes;
+
+  for (size_t i = 0; i < data.tracks.size(); i++) {
+    uint32_t currentTime = 0;
+
+    auto visitMidi = [&currentTime, &notes,
+                      &noteOnTimes](MIDIEvent& e) mutable {
+      currentTime += e.deltaTime;
+      const bool hasNoteOnStatus = (e.status & 0b11110000) == 0b10010000;
+      const bool hasNoteOffStatus = (e.status & 0b11110000) == 0b10000000;
+
+      if (!(hasNoteOnStatus || hasNoteOffStatus)) {
+        return;
+      }
+
+      const uint8_t key = e.data.at(0);
+      const uint8_t velocity = e.data.at(1);
+      const bool isNoteOn = hasNoteOnStatus && velocity > 0;
+      const bool isNoteOff =
+          (hasNoteOnStatus && velocity == 0) || hasNoteOffStatus;
+
+      if (isNoteOn) {
+        noteOnTimes.insert_or_assign(e.data.at(0), currentTime);
+      }
+      if (isNoteOff) {
+        notes.push_back({.key = key,
+                         .timeStart = noteOnTimes.at(key),
+                         .timeEnd = currentTime});
+      }
+    };
+
+    auto visitor = overload{
+        visitMidi,
+        [&currentTime](MetaEvent& e) mutable { currentTime += e.deltaTime; },
+        [](SysExEvent&) { /* ignore */ }};
+
+    for (TrackEvent e : data.tracks.at(i).events) {
+      std::visit(visitor, e);
+    }
+  }
+
+  return notes;
+}
+
+class Window {
+  static constexpr auto FLAGS =
+      SDL_WINDOW_RESIZABLE | SDL_WINDOW_UTILITY | SDL_WINDOW_RESIZABLE;
+
+ public:
+  explicit Window(int w, int h)
+      : window(SDL_CreateWindow("can", 0, 0, w, h, FLAGS), SDL_DestroyWindow),
+        surface(SDL_GetWindowSurface(window.get()), SDL_FreeSurface) {};
+
+  std::unique_ptr<SDL_Window, std::function<void(SDL_Window*)>> window;
+  std::unique_ptr<SDL_Surface, std::function<void(SDL_Surface*)>> surface;
+  void updateSurface() { SDL_UpdateWindowSurface(window.get()); }
+};
+
+class App {
+ public:
+  App() { initSDL(); }
+  ~App() { SDL_Quit(); }
+
+ private:
+  void initSDL() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      throw std::runtime_error(SDL_GetError());
+    }
+    SDL_GetCurrentDisplayMode(0, &m_mode);
+  }
+
+ public:
+  void run() {
+    int windowWidth = m_mode.w * 0.38;
+    int windowHeight = m_mode.h * 0.38;
+
+    // PREPARE
+
+    MidiParser::Parser parser;
+    auto data = parser.parse("tmp/twinkle.mid");
+    auto qNotes = quantizeMIDI(data);
+
+    auto cmpy = [](const QuantizedNote& a, const QuantizedNote& b) {
+      return a.key < b.key;
+    };
+    auto cmpx = [](const QuantizedNote& a, const QuantizedNote& b) {
+      return a.timeStart > b.timeStart;
+    };
+
+    auto highestNote = *std::max_element(qNotes.begin(), qNotes.end(), cmpy);
+    auto lowestNote = *std::min_element(qNotes.begin(), qNotes.end(), cmpy);
+    /* auto leftMostNote = *std::max_element(qNotes.begin(), qNotes.end(), cmpx); */
+    auto rightMostNote = *std::min_element(qNotes.begin(), qNotes.end(), cmpx);
+
+    uint8_t gridSizeY = highestNote.key - lowestNote.key;
+    int gridHeight = windowHeight / (int)gridSizeY;
+
+    // RENDER
+
+    Window w(windowWidth, windowHeight);
+
+    SDL_FillRect(w.surface.get(), nullptr,
+                 SDL_MapRGB(w.surface->format, 0, 0, 0));
+
+    for (auto note : qNotes) {
+      SDL_Rect r{.x = (int)map(note.timeStart, 0, rightMostNote.timeEnd, 0,
+                               windowWidth, false),
+                 .y = (int)map(note.key, lowestNote.key, highestNote.key,
+                               windowHeight - gridHeight, 0) +
+                      2,
+                 .w = (int)map(note.timeEnd - note.timeStart, 0,
+                               rightMostNote.timeEnd, 0, windowWidth),
+                 .h = gridHeight - 4};
+      SDL_FillRect(w.surface.get(), &r,
+                   SDL_MapRGB(w.surface->format, 0xFF, 0xFF, 0xFF));
+    }
+
+    w.updateSurface();
+
+    SDL_Event e;
+    while (true) {
+      SDL_PollEvent(&e);
+      if (e.type == SDL_KEYDOWN) {
+        if (e.key.keysym.sym == SDL_KeyCode::SDLK_ESCAPE ||
+            e.key.keysym.sym == SDL_KeyCode::SDLK_q) {
+          break;
+        }
+      }
+      if (e.type == SDL_QUIT) {
+        break;
+      }
+    }
+  }
+
+  SDL_DisplayMode m_mode;
+};
+
+int main() {
+  App app;
+  app.run();
+}
